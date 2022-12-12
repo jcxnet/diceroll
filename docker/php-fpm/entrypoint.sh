ea6b304b98523c74e575304bf632716e5fa2c031
Juan Carlos <juancarlos.iberico@digimobil.es>
2022-12-12 17:23:58 +0100

    Docker config - create docker-compose script to generate environment for the project

---
+++ docker/php-fpm/entrypoint.sh
@@ -0,0 +1,18 @@
+#!/bin/sh
+
+set -e
+
+if [ "${1#-}" != "$1" ]; then
+  set -- php-fpm "$@"
+fi
+
+FILE=composer.json
+if [ -f "$FILE" ]; then
+    echo "Installing libraries from composer"
+    composer install --prefer-dist --no-progress --no-interaction
+else
+    echo "$FILE not found."
+fi
+
+
+exec docker-php-entrypoint "$@"
